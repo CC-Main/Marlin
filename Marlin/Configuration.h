df8cab0d45d7b6222ab9c84b7f72558142b0a80a
CC-Main <41975448+CC-Main@users.noreply.github.com>
2023-02-14 21:14:57 -0600

    Set E-Steps for NG
    Set the E-steps to the default value recommended by Micro Swiss for the NG extruder.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1224,7 +1224,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 93 } //CCM
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 400 } //CCM
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
